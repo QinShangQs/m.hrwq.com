0382d16c82d352757869594d535cdaee436981a8
liziqiang <914134926@qq.com>
2017-08-22 08:37:13 +0800

    被分享着注册都有奖励

--- app/Listeners/RegisterPeopleEventListener.php
+++ app/Listeners/RegisterPeopleEventListener.php
@@ -22,7 +22,7 @@ class RegisterPeopleEventListener {
 				'url' => route('user'),
 				'topcolor' => '#f7f7f7',
 				'data' => [
-						'first' => '您已成功推荐一名家长加入和润万青父母学院，送您7天和会员奖励。'."\n",
+						'first' => '您已成功推荐一名家长注册和润万青父母学院，送您7天和会员奖励。'."\n",
 						'keyword1'=> $user->nickname."\n",
 						'keyword2'=> date('Y-m-d H:i:s')."\n",
 						'remark'=> '点击此处查看我的和会员期限'
