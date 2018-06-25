2c2eeafe80ac61bfae2ec24f207a2b1c9b2ea2cc
qinshangQs <liziqiang.qs@outlook.com>
2018-06-25 11:40:06 +0800

    fxied

--- app/Http/Controllers/VcourseController.php
+++ app/Http/Controllers/VcourseController.php
@@ -56,12 +56,10 @@ class VcourseController extends Controller {
         }
 
         $wx_js = Wechat::js();
-        if (config('app.debug') === false) {
-            if (preg_match('/^win/i', PHP_OS)) {
-                $data = file_get_contents('E:/sug_link.log');
-            } else {
-                $data = file_get_contents('/mnt/sug_link.log');
-            }
+        if (preg_match('/^win/i', PHP_OS)) {
+            $data = file_get_contents('E:/sug_link.log');
+        } else {
+            $data = file_get_contents('/mnt/sug_link.log');
         }
         if (!empty($data)) {
             list($telecast, $foreshow) = explode("\n", $data);
