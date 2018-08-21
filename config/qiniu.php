c833ab50ad71d0ab6c1806a776f0acb538183314
qinshangQs <liziqiang.qs@outlook.com>
2018-08-21 15:29:25 +0800

    fixed

--- config/qiniu.php
+++ config/qiniu.php
@@ -9,7 +9,7 @@ return [
     'PIPELINE'  => 'hrwq20160811',
     'FREE_TIME'  => '60',
     'COVER_TIME'  => '30',
-    'DOMAIN' => 'http://oflmtu502.bkt.clouddn.com/',
+    'DOMAIN' => 'http://oflm462i7.qnssl.com/',
     'DOMAIN_USERCOVER' => 'http://photos.partner.hrwq.com/',
     'DOMAIN_DEVELOP' => 'http://pbong34w2.bkt.clouddn.com/',
     'AK'  => 'CNSLO6Zft_HXWPuVve4fvH98kMbfIdlJBJaAlJCA',
