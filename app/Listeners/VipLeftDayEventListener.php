9d4312834a89885c31ae9d4c6a43457762dda84a
qinshangQs <liziqiang.qs@outlook.com>
2019-02-20 23:43:01 +0800

    fixed

--- app/Listeners/VipLeftDayEventListener.php
+++ app/Listeners/VipLeftDayEventListener.php
@@ -14,15 +14,21 @@ class VipLeftDayEventListener {
     public function onTemplate($event) {
         $left_day = $event->user->left_day;
         $first = "";
-        if ($left_day === 3) {
-            $first = "您的和会员有效期仅剩3天，到期后将无法享受会员权限，请及时开通会员或进行续费。";
-        } else if ($left_day === 1) {
+//        if ($left_day === 3) {
+//            $first = "您的和会员有效期仅剩3天，到期后将无法享受会员权限，请及时开通会员或进行续费。";
+//        } else if ($left_day === 1) {
+//            $first = "您的和会员有效期仅剩最后1天，到期后将无法享受会员权限，请及时开通会员或进行续费。";
+//        } else if ($left_day === 0) {
+//            $first = "您的和会员已到期，已无法享受会员权限，请及时开通会员或进行续费。";
+//        } else {
+//            return; //未开通会员
+//        }
+        if ($left_day === 1) {
             $first = "您的和会员有效期仅剩最后1天，到期后将无法享受会员权限，请及时开通会员或进行续费。";
-        } else if ($left_day === 0) {
-            $first = "您的和会员已到期，已无法享受会员权限，请及时开通会员或进行续费。";
         } else {
-            return; //未开通会员
+            return;
         }
+
         $result = send_sms([$event->user->mobile], $first);
         Log::info($result);
     }
